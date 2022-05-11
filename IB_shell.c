a109ba3fadd25edb563b6d9946174b90fc3b322c
Boakye2 <nanaboakyefred2@gmail.com>
2022-05-12 00:02:16 +0200

    update

--- IB_shell.c
+++ IB_shell.c
@@ -1,36 +1,69 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+
 #include "main.h"
-#include <string.h>
 /**
  * main - function that execute our shell
  */
 int main(int argc, char *argv[], char *en[])
-{
-	char c = '\0';
-	char *val = (char *)malloc(sizeof(char) * 100);
-	printf("\nIB_shell ");
-	while (c != EOF)
-	{
+{char c;
+	int i, fd;
+	char *tmp = (char *)malloc(sizeof(char) * 100);
+	char *path_str = (char *)malloc(sizeof(char) * 256);
+	char *cmd = (char *)malloc(sizeof(char) * 100);
+	
+	signal(SIGINT, SIG_IGN);
+	signal(SIGINT, handle_signal);
+
+	copy_envp(envp);
+	get_path_string(my_envp, path_str);
+	insert_path_str_to_search(path_str);
+
+	if(fork() == 0) {
+		execve("/usr/bin/clear", argv, my_envp);
+		exit(1);
+	} else {
+		wait(NULL);
+	}
+	printf("[MON_SHELL ] ");
+	fflush(stdout);
+	while(c != EOF) {
 		c = getchar();
-		switch (c)
-		{
-			case '\n': if (val[0] == '\0'){
-					   printf("IB_shell ");
+		switch(c) {
+			case '\n': if(tmp[0] == '\0') {
+					   printf("[MON_SHELL ] ");
 				   } else {
-					 if (fork() == 0){
-						 system(val);
-						 exit(1);
+					   fill_argv(tmp);
+					   strncpy(cmd, my_argv[0], strlen(my_argv[0]));
+					   strncat(cmd, "\0", 1);
+					   if(index(cmd, '/') == NULL) {
+						   if(attach_path(cmd) == 0) {
+							   call_execve(cmd);
+						   } else {
+							   printf("%s: command not found\n", cmd);
+						   }
+					   } else {
+						   if((fd = open(cmd, O_RDONLY)) > 0) {
+							   close(fd);
+							   call_execve(cmd);
+						   } else {
+							   printf("%s: command not found\n", cmd);
+						   }
+					   }
+					   free_argv();
+					   printf("[MON_SHELL ] ");
+					   bzero(cmd, 100);
 				   }
-				   }
-				   printf("\nIB_shell ");
+				   bzero(tmp, 100);
 				   break;
-			default:strncat(val, &c, 1);
-				break;
+			default: strncat(tmp, &c, 1);
+				 break;
 		}
 	}
-	printf("%s",val);
+	free(tmp);
+	free(path_str);
+	for(i=0;my_envp[i]!=NULL;i++)
+		free(my_envp[i]);
+	for(i=0;i<10;i++)
+		free(search_path[i]);
 	printf("\n");
-	return (0);
+	return 0;
 }
--- main.h
+++ main.h
@@ -1,10 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <signal.h>
+#include <string.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <ctype.h>
+#include <errno.h>
+#include <sys/stat.h>
 #include <fcntl.h>
-void e(char *var);
+extern int errno;
+
+typedef void (*sighandler_t)(int);
+static char *my_argv[100], *my_envp[100];
+static char *search_path[10];
+
+void insert_path_str_to_search(char *path_str);
+void fill_argv(char *tmp_argv);
 
 #endif
