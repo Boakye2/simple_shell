8cea87473f5ba153f02e881dc762c5372529d710
Boakye2 <nanaboakyefred2@gmail.com>
2022-05-12 00:18:47 +0200

    new

--- IB_shell.c
+++ IB_shell.c
@@ -3,67 +3,30 @@
 /**
  * main - function that execute our shell
  */
-int main(int argc, char *argv[], char *en[])
-{char c;
-	int i, fd;
-	char *tmp = (char *)malloc(sizeof(char) * 100);
-	char *path_str = (char *)malloc(sizeof(char) * 256);
-	char *cmd = (char *)malloc(sizeof(char) * 100);
-	
-	signal(SIGINT, SIG_IGN);
-	signal(SIGINT, handle_signal);
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
 
-	copy_envp(envp);
-	get_path_string(my_envp, path_str);
-	insert_path_str_to_search(path_str);
+char c = '\0';
 
-	if(fork() == 0) {
-		execve("/usr/bin/clear", argv, my_envp);
-		exit(1);
-	} else {
-		wait(NULL);
-	}
-	printf("[MON_SHELL ] ");
-	fflush(stdout);
+
+
+int main()
+{
+	printf("IB_shell  ");
 	while(c != EOF) {
 		c = getchar();
 		switch(c) {
-			case '\n': if(tmp[0] == '\0') {
-					   printf("[MON_SHELL ] ");
-				   } else {
-					   fill_argv(tmp);
-					   strncpy(cmd, my_argv[0], strlen(my_argv[0]));
-					   strncat(cmd, "\0", 1);
-					   if(index(cmd, '/') == NULL) {
-						   if(attach_path(cmd) == 0) {
-							   call_execve(cmd);
-						   } else {
-							   printf("%s: command not found\n", cmd);
-						   }
-					   } else {
-						   if((fd = open(cmd, O_RDONLY)) > 0) {
-							   close(fd);
-							   call_execve(cmd);
-						   } else {
-							   printf("%s: command not found\n", cmd);
-						   }
-					   }
-					   free_argv();
-					   printf("[MON_SHELL ] ");
-					   bzero(cmd, 100);
-				   }
-				   bzero(tmp, 100);
+			case '\n':  if(tmp[0] == '\0') {
+					   printf("IB_shell  ");
+				   } else {}
 				   break;
 			default: strncat(tmp, &c, 1);
 				 break;
 		}
 	}
-	free(tmp);
-	free(path_str);
-	for(i=0;my_envp[i]!=NULL;i++)
-		free(my_envp[i]);
-	for(i=0;i<10;i++)
-		free(search_path[i]);
+	f
 	printf("\n");
 	return 0;
 }
