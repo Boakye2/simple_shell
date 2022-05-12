778ae3f415d73b7a6e5a756f7f492cd0cee08e03
Boakye2 <nanaboakyefred2@gmail.com>
2022-05-12 22:35:56 +0200

    the header

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <ctype.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#endif
