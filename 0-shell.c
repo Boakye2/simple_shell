0262081876180a2754fcb2a416cd453f68642847
Hegeldirkk <hegeldirkk@gmail.com>
2022-05-19 19:04:45 +0000

    change my code

--- 0-shell.c
+++ 0-shell.c
@@ -1,29 +1,49 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
  * main - program that prints "$ ".
- * Return: 0;
+ * Return: a;
  */
 
 int main(void)
 {
-	char *buffer;
 	size_t buflen = 32;
-	size_t valeur;
+	char *buffer, *argv[] = {"/bin/ls", NULL}, cmd[] = "/bin/ls";
 
-	buffer = malloc(buflen * sizeof(char));
+	buffer = (char *)malloc(buflen * sizeof(char));
 	if (!buffer)
 	{
 		perror("impossible d'allouer");
 	}
-	for (int i = 0; i < 5; i++)
+	printf("#cisfun$");
+	getline(&buffer, &buflen, stdin);
+	while (*buffer != EOF)
 	{
-		printf("$ ");
-		valeur = getline(&buffer, &buflen, stdin);
-		printf("%zu \n", valeur);
-		printf("%s\n", buffer);
+		if (cmd[0] == *buffer)
+		{
+			int i;
+
+			if (fork() == 0)
+			{
+				i = execve(cmd, argv, NULL);
+				if (i < 0)
+				{
+					printf("./shell: No such file or directory\n");
+				}
+			}
+			else
+			{
+				wait(NULL);
+			}
+			printf("#cisfun$");
+			getline(&buffer, &buflen, stdin);
+		}
+		else
+		{
+			printf("./shell: No such file or directory\n");
+		}
+	printf("#cisfun$");
+	getline(&buffer, &buflen, stdin);
 	}
 	return (0);
 }
